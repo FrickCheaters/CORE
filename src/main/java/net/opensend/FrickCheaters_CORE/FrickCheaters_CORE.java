2e47875cbbb55c43cbd7ffe88df2686cdc74484e
Ollie <ollie@auntodev.com>
2021-02-21 02:10:02 +0000

    Add files via upload

--- src/main/java/net/opensend/FrickCheaters_CORE/FrickCheaters_CORE.java
+++ src/main/java/net/opensend/FrickCheaters_CORE/FrickCheaters_CORE.java
@@ -1,9 +1,12 @@
 package net.opensend.FrickCheaters_CORE;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class FrickCheaters_CORE extends JavaPlugin {
-    public static final String version = "1.0";
+    public static final String version = "2021.1.1-pre";
     public static final long apiVersion = 1;
 
     @Override
@@ -23,5 +26,15 @@ public class FrickCheaters_CORE extends JavaPlugin {
     public void onDisable () {
         getLogger().warning("Disable request received: The server is either restarting, or a plugin reload has been initiated.");
         getLogger().warning("Even though this plugin DOES support reloading, other plugins, including FrickCheaters, may have issues after reloads.");
+    }
+
+    @Override
+    public boolean onCommand (CommandSender sender, Command command, String label, String[] args) {
+        sender.sendMessage(color("&f&lYou're running &c&lFrickCheaters &6&lCORE\n&7Version: v" + version + "\n\n&bhttps://github.com/FrickCheaters/CORE"));
+        return true;
+    }
+
+    private String color (String string) {
+        return ChatColor.translateAlternateColorCodes('&', string);
     }
 }
